00b61eecd07b981bbd5ae427c4276e582e75c855
Med Mahmoud El Joud (MMJ) <medmahmoudjoud@gmail.com>
2023-04-01 20:03:21 +0000

    my

--- src/main/main-section.jsx
+++ src/main/main-section.jsx
@@ -3,10 +3,19 @@ import firstProduct from '../assets/images/image-product-1-thumbnail.jpg'
 import secondProduct from '../assets/images/image-product-2-thumbnail.jpg'
 import thirdProduct from '../assets/images/image-product-3-thumbnail.jpg'
 import fourProduct from '../assets/images/image-product-4-thumbnail.jpg'
+import { useState } from 'react'
 
 import f from '../assets/images/image-product-1.jpg'
 import Counter from './counter'
 function MainSection() {
+    const [count, setCount] = useState(0);
+
+    function plus() {
+        setCount(count + 1)
+    }
+    function minus() {
+        setCount(count - 1)
+    }
     return (
         <section className='container'>
             <div className='images-wrapper'>
@@ -27,7 +36,7 @@ function MainSection() {
                 <h1 className='price'> $125.00<span>50%</span></h1>
                 <h2 className='old-price'>$250.00</h2>
                 <div className="counting-wrapper">
-                    <Counter />
+                    <Counter minus={minus} plus={plus} Count={count}/>
                     <button className='add-btn'>
                     <svg className='add-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
                         Add to cart
---
+++ src/main/popover.jsx
@@ -0,0 +1,18 @@
+import * as Popover from '@radix-ui/react-popover';
+
+
+function Popover() {
+  return (
+    <Popover.Root>
+    <Popover.Trigger />
+    <Popover.Anchor />
+    <Popover.Portal>
+      <Popover.Content>
+        <Popover.Close />
+        <Popover.Arrow />
+      </Popover.Content>
+    </Popover.Portal>
+  </Popover.Root>  )
+}
+
+export default Popover
